bf713995eee80c84c755c1e43696190110dc7c12
cls1991 <tzk19910406@hotmail.com>
2016-09-19 20:11:02 +0800

    test go slice & print

--- hello/hello.go
+++ hello/hello.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-    "fmt"
-    "github.com/cls1991/go-learning/stringutil"
+	"fmt"
+	"github.com/cls1991/go-learning/stringutil"
 )
 
 func main() {
-    fmt.Printf(stringutil.Reverse("!oG ,olleH"))
+	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
 }
