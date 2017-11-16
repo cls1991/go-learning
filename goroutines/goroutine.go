a1b5d380be4ab716ceba9ae223efe097220c8652
cls1991 <tzk19910406@hotmail.com>
2017-11-16 21:04:36 +0800

    add goroutine samples

--- goroutines/goroutine.go
+++ goroutines/goroutine.go
@@ -6,25 +6,25 @@ import (
 	"time"
 )
 
-func main() {
-	go say("golang", 1)
-	say("hello", 3)
-
-	tick := time.Tick(100 * time.Microsecond)
-	boom := time.After(500 * time.Microsecond)
-	for {
-		select {
-		case <-tick:
-			fmt.Println("tick.")
-		case <-boom:
-			fmt.Println("BOOM!")
-			return
-		default:
-			fmt.Println("     .")
-			time.Sleep(50 * time.Microsecond)
-		}
-	}
-}
+//func main() {
+//	go say("golang", 1)
+//	say("hello", 3)
+//
+//	tick := time.Tick(100 * time.Microsecond)
+//	boom := time.After(500 * time.Microsecond)
+//	for {
+//		select {
+//		case <-tick:
+//			fmt.Println("tick.")
+//		case <-boom:
+//			fmt.Println("BOOM!")
+//			return
+//		default:
+//			fmt.Println("     .")
+//			time.Sleep(50 * time.Microsecond)
+//		}
+//	}
+//}
 
 func say(s string, t time.Duration) {
 	for i := 0; i < 3 ; i++ {
